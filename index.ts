4c6321c0a1452f14ee11f7c4766a3af377f3af43
aiya44 <ychen0404@icloud.com>
2020-05-23 22:43:28 -0700

    added emoji reactions

--- index.ts
+++ index.ts
@@ -5,35 +5,46 @@ client.once("ready", () => {
   console.log("Ready!");
 });
 
-client.on("message", (message) => {
-
+client.on("message", (message: any) => {
   // console.log("these r rollles", message.channel.guild.roles);
   if (message.author.bot) {
     return;
   } else {
-    message.reply(message.content);
+    message
+      .reply(
+        `Hi, please let us know which role you are intested in: \n :pig: pig \n :frog: frog \n :chicken: chicken \n`
+      )
+      .then((result: any) => {
+        result.react("🐷");
+        result.react("🐸");
+        result.react("🐔");
+      });
   }
 
-  console.log(message.guild.emojis); 
-  if (message.content === '!react') {
-    message.react('😄');
+  // console.log(message.guild.emojis);
+  // if (message.content === "!react") {
+  //   message.react("😄");
+  // }
+
+  // console.log(message);
+});
+
+client.on("messageReactionAdd", async (reaction, user) => {
+  if (reaction.partial) {
+    try {
+      await reaction.fetch();
+    } catch (error) {
+      console.log("Something went wrong when fetching the message: ", error);
+      return;
+    }
   }
 
-
-
-  if (message.author.bot) {
-    return;
-  } else {
-    message.reply(message.content);
-  }
-
-
-
-
-
-
-
-
+  // console.log(
+  //   `${reaction.message}'s message "${reaction.message.content}" gained a reaction!`
+  // );
+  // console.log(
+  //   `this is reactions: ${reaction} `
+  // );
 });
 
 client.login("NzEzODMxMDQ5OTAxMTEzMzk3.Xsmj_w.-Q5kFMojzMOeK4T4Qao6QN_y7wY");
